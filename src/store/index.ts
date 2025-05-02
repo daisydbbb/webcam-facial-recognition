7009c3e078ac947090bc25a999666f49d6d7b477
“Daisy <daisyding666@gmail.com>
2025-05-02 11:52:16 -0700

    stage1 complete, set up

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+// We will add slices later
+
+
+// redux store
+export const store = configureStore({
+  reducer: {
+    // webcam: webcamReducer,
+    // faces: facesReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
